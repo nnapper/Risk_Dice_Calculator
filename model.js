49fa1b692d6ee6d610a0f106bdaf01dc32c0b0d9
Alvin Ao <alvinao@mini.lan>
2021-07-29 11:17:22 -0700

    added a html version and a p5js version. there is also manual rolling and blitz attacks

---
+++ model.js
@@ -0,0 +1,13 @@
+const global = (function () {
+  const attackingTroops = 1;
+  const defendingTroops = 1;
+  const attackingDice = 1;
+  const defendingDice = 1;
+
+  return {
+    ATTACKING_TROOPS: attackingTroops,
+    DEFENDING_TROOPS: defendingTroops,
+    ATTACKING_DICE: attackingDice,
+    DEFENDING_DICE: defendingDice,
+  };
+})();
